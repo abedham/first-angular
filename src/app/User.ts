6e8a060fad1e81da7d575e92e8fbc3a3c6a70e81
Abdelrahman <abed.ha15@gmail.com>
2018-07-19 13:57:36 +0300

    Create User module

---
+++ src/app/User.ts
@@ -0,0 +1,8 @@
+export class User {
+    name: String;
+    email: String;
+    constructor(name, email) {
+      this.name = name;
+      this.email = email;
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { User } from './User';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  user = new User("Abdlerahman", "abed.ha15@gmail.com");
+  title = 'app: ' + this.user.name;
 }
